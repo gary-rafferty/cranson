1b8441a8b7997679de6f806f68fda3c54096122b
Gary Rafferty <gary.rafferty@gmail.com>
2016-12-11 21:28:11 +0000

    wip: print output as it gets parsed

--- lib/cranson/parser.rb
+++ lib/cranson/parser.rb
@@ -1,12 +1,16 @@
 module Cranson
   class Parser < Nokogiri::XML::SAX::Document
     def start_element(name, attrs = [])
+      p name
     end
 
     def characters(str)
+      return if str.include? "\n"
+      p str
     end
 
     def end_element(name)
+      p name
     end
   end
 end
